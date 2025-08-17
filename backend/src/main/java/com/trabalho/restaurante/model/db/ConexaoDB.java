c92859d6bf9ef25969952da85eb3302d143991df
RuanPablo-7 <ToyaMON777@outlook.com>
2025-08-17 13:00:11 -0300

    sqlite add

--- backend/src/main/java/com/trabalho/restaurante/model/db/ConexaoDB.java
+++ backend/src/main/java/com/trabalho/restaurante/model/db/ConexaoDB.java
@@ -8,16 +8,9 @@ public class ConexaoDB {
 
     public static Connection getConexao() throws ClassNotFoundException, SQLException {
 
-        String nomeBaseDados = System.getenv("DATABASE_USERNAME");
-        String porta = System.getenv("DATABASE_PORT");
-        String enderecoBaseDados = String.format( "jdbc:postgresql://restaurante_bd_backend_user:gyMatGG7CWnJYtJBkWxVgW5c4OtilnQz@dpg-d2gus0gdl3ps73fofj10-a/restaurante_bd_backend", porta, nomeBaseDados);
+        String url = "jdbc:sqlite:src/main/resources/meubanco.db";
 
-        String login = System.getenv("DATABASE_USER");
-        String senha = System.getenv("DATABASE_PASSWORD");
-
-        Class.forName("org.postgresql.Driver");
-
-        return DriverManager.getConnection(enderecoBaseDados, login, senha);
+        return DriverManager.getConnection(url);
 
     }
 }
